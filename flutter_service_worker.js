8b6b7929d110f75aba4eaa9ae18c94810b8ce470
Rajib Lochan Nandi <adityanandi550@gmail.com>
2022-03-02 17:39:06 +0530

    final-beta

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,9 +24,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "25d3db590aa4a3cde934f5abe3b4fc5a",
-"/": "25d3db590aa4a3cde934f5abe3b4fc5a",
-"main.dart.js": "50cc1c8cdf4790f6327acae7fff76965",
+"index.html": "951d410ab872787bae2662e745acadfe",
+"/": "951d410ab872787bae2662e745acadfe",
+"main.dart.js": "e031664674a1bfa61f4b2423e83f54a7",
 "manifest.json": "63a57781c4200171960190bc23489f3c",
 "version.json": "ed3459d45c29688c4aa900c55d38f7cb"
 };
